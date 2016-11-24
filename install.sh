1ae1762faff72b0a16a1f3920b85d36e9421fcd2
Will Highducheck <will.highducheck@gmail.com>
2016-11-23 23:23:10 -0700

    Install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+ln -sf $PWD/.ackrc                                 $HOME/.ackrc                                      
+ln -sf $PWD/.bash_functions                        $HOME/.bash_functions                             
+ln -sf $PWD/.bash_profile                          $HOME/.bash_profile                               
+ln -sf $PWD/.bashrc                                $HOME/.bashrc                                     
+ln -sf $PWD/.ctags                                 $HOME/.ctags                                      
+ln -sf $PWD/.git-prompt.sh                         $HOME/.git-prompt.sh                              
+ln -sf $PWD/.gitignore                             $HOME/.gitignore                                  
+ln -sf $PWD/.gitignore_global                      $HOME/.gitignore_global                           
+ln -sf $PWD/.gitmodules                            $HOME/.gitmodules                                 
+ln -sf $PWD/.irbrc                                 $HOME/.irbrc                                      
+ln -sf $PWD/.midje.clj                             $HOME/.midje.clj                                  
+ln -sf $PWD/.pryrc                                 $HOME/.pryrc                                      
+ln -sf $PWD/.pythonrc                              $HOME/.pythonrc                                   
+ln -sf $PWD/.railsrc                               $HOME/.railsrc                                    
+ln -sf $PWD/.tmux.conf                             $HOME/.tmux.conf                                  
+mkdir -p $HOME/.tmuxinator/
+ln -sf $PWD/.tmuxinator/                           $HOME/.tmuxinator/                                
+mkdir -p $HOME/.vim/
+ln -sf $PWD/.vim/                                  $HOME/.vim/                                       
+ln -sf $PWD/.vimrc                                 $HOME/.vimrc                                      
+mkdir -p $HOME/gnome-terminal-colors-solarized/
+ln -sf $PWD/gnome-terminal-colors-solarized/       $HOME/gnome-terminal-colors-solarized/            
