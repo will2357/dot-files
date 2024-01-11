1d9e26cb65ef931f42ba1b0ba78af7e5c1b4afbf
will2357 <will.highducheck@gmail.com>
2024-01-11 13:47:21 -0500

    don't name installation script 'install.sh'

---
+++ dot_install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+curr_dir=$PWD
+echo "$curr_dir"
+
+target_dir=$HOME
+echo "$target_dir"
+
+link_dot_files () {
+    echo "Linking dot files (ln -sf $curr_dir/filename $target_dir/.filename)"
+    echo "Backups will be made as follows: .filename filenameBAK"
+    #ln -sf $PWD/.ackrc                                 $HOME/.ackrc
+    #ln -sf $PWD/.bash_functions                        $HOME/.bash_functions
+    #ln -sf $PWD/.bash_profile                          $HOME/.bash_profile
+    #ln -sf $PWD/.bashrc                                $HOME/.bashrc
+    #ln -sf $PWD/.ctags                                 $HOME/.ctags
+    #ln -sf $PWD/.git-prompt.sh                         $HOME/.git-prompt.sh
+    #ln -sf $PWD/.gitignore                             $HOME/.gitignore
+    #ln -sf $PWD/.gitignore_global                      $HOME/.gitignore_global
+    #ln -sf $PWD/.gitmodules                            $HOME/.gitmodules
+    #ln -sf $PWD/.irbrc                                 $HOME/.irbrc
+    #ln -sf $PWD/.midje.clj                             $HOME/.midje.clj
+    #ln -sf $PWD/.pryrc                                 $HOME/.pryrc
+    #ln -sf $PWD/.pythonrc                              $HOME/.pythonrc
+    #ln -sf $PWD/.railsrc                               $HOME/.railsrc
+    #ln -sf $PWD/.tmux.conf                             $HOME/.tmux.conf
+    #mkdir -p $HOME/.vim/
+    #ln -sf $PWD/.vim/                                  $HOME/.vim/
+    #ln -sf $PWD/.vimrc                                 $HOME/.vimrc
+    #mkdir -p $HOME/gnome-terminal-colors-solarized/
+    #ln -sf $PWD/gnome-terminal-colors-solarized/       $HOME/gnome-terminal-colors-solarized/
+}
+
+
+
+while true; do
+    read -p "Do you wish to link your dot files to your home directory (yes/no)? " yn
+    case $yn in
+        [Yy]* ) link_dot_files; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
