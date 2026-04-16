2acf94e66d322c39dc36f8f0b2c909ae03a02d52
will2357 <will.highducheck@gmail.com>
2026-04-16 17:50:58 -0400

    fix: add local to function variables in _shared_functions.sh
    Variables in prn_error, prn_success, prn_note, and get_confirmation
    were leaking into the caller's scope. Declare them local.

--- _shared_functions.sh
+++ _shared_functions.sh
@@ -5,26 +5,30 @@ set -eu
 color_normal=$(tput sgr0)
 
 prn_error () {
-    error_message=$1
+    local error_message=$1
+    local color_red
     color_red=$(tput setaf 1)
     printf "\n%s\n" "${color_red}$error_message${color_normal}"
 }
 
 prn_success () {
-    success_message=$1
+    local success_message=$1
+    local color_green
     color_green=$(tput setaf 2)
     printf "\n%s\n" "${color_green}$success_message${color_normal}"
 }
 
 prn_note () {
+    local color_blue
     color_blue=$(tput setaf 4)
-    note_message=$1
+    local note_message=$1
     printf "\n%s\n" "${color_blue}$note_message${color_normal}"
 }
 
 get_confirmation () {
     [ -z "$1" ] && prn_error "Must include a message for the user. Exiting." && exit 1
-    message=$1
+    local message=$1
+    local bold_message
     bold_message="$(tput bold)$message (y/N) ${color_normal}"
 
     read -r -p "$bold_message" yn
